6e8d86520900ced2ba3c64c248b64a6cb02e17cd
Matt Maloney <mwmaloney88@gmail.com>
2013-06-02 14:09:50 -0400

    Added validations and relations to models

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,20 @@
 class App < ActiveRecord::Base
   attr_accessible :codebase_url, :description, :email, :title, :url
+
+  has_many :comments
+
+  validates_presence_of :codebase_url
+  validates_presence_of :description
+  validates_presence_of :email
+  validates_presence_of :title
+  validates_presence_of :url
+
+
+  validates_uniqueness_of :title
+  validates_uniqueness_of :codebase_url
+  validates_uniqueness_of :url 
+
+  validates_format_of :email, with: /@/
+
+  
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,15 @@
 class Comment < ActiveRecord::Base
   attr_accessible :app_id, :comment, :email, :first_name, :last_name
+
+  validates_presence_of :comment
+  validates_presence_of :email
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+
+  validates :comment, length:{ minimum: 1, too_short: "You need to enter a comment"}
+  validates :first_name, length:{ minimum: 2, too_short: "Name must be atleast 2 characters"}
+  validates :last_name, length:{minimum: 2, too_short: "Name must be atleast 2 characters"}
+
+
+  belongs_to :app
 end
