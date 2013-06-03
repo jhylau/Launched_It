9739887872d08550a537bec676c43e67af495220
Jide Fajobi <wazogear@gmail.com>
2013-06-03 16:13:36 -0400

    delete test directory and add rspec

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130531201759) do
+ActiveRecord::Schema.define(:version => 20130531201401) do
 
   create_table "apps", :force => true do |t|
     t.string   "title"
@@ -21,16 +21,6 @@ ActiveRecord::Schema.define(:version => 20130531201759) do
     t.string   "email"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
-  end
-
-  create_table "comments", :force => true do |t|
-    t.string   "email"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.text     "comment"
-    t.integer  "app_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
   end
 
 end
